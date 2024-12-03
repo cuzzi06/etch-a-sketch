208f6c268f132746b7c2253cc4cff9f3bee8afe2
Gianfranco <120363150+cuzzi06@users.noreply.github.com>
2024-12-03 12:16:23 -0500

    Agregar funcionalidad para generar cuadrícula dinámica y asignar efecto hover
    - Permitir al usuario especificar el tamaño de la cuadrícula a través de un prompt.
    - Generar dinámicamente la cuadrícula basada en el tamaño ingresado.
    - Limpiar el contenedor antes de crear una nueva cuadrícula.
    - Aplicar efecto hover a cada celda de la cuadrícula generada.
    - Validar el tamaño máximo permitido para evitar errores.

--- script.js
+++ script.js
@@ -1,24 +1,36 @@
-//Selector de container en index
+//Selectores de index
 const containerDiv = document.querySelector("#container");
+const promptButton = document.querySelector("#promptButton");
 
-//Funcion para hacer filas de 16 cuadrados
-function rowSquereDiv() {
-  for (let i = 1; i <= 16; i++) {
-    const gridSquareDiv = document.createElement("div");
-    gridSquareDiv.setAttribute("class", "squareDiv");
-    containerDiv.appendChild(gridSquareDiv);
+let numberGridSize;
+
+promptButton.addEventListener("click", () => {
+  numberGridSize = +prompt("Ingrese número de cuadrados por lado");
+
+  containerDiv.innerHTML = "";
+
+  if (numberGridSize <= 100) {
+    function createGrid() {
+      for (let i = 1; i <= numberGridSize * numberGridSize; i++) {
+        const gridSquareDiv = document.createElement("div");
+        gridSquareDiv.setAttribute("class", "squareDiv");
+        containerDiv.appendChild(gridSquareDiv);
+      }
+    }
+
+    createGrid();
+    assignHoverEffect();
+  } else {
+    alert("Por favor, ingrese un número menor o igual a 100");
   }
-}
-
-//Función para hacer 16 filas
-for (let i = 1; i <= 16; i++) {
-  rowSquereDiv();
-}
+});
 
 //Hover
-const squares = document.querySelectorAll(".squareDiv");
-squares.forEach((squareHover) => {
-  squareHover.addEventListener("mouseover", () => {
-    squareHover.style.backgroundColor = "black";
+function assignHoverEffect() {
+  const squares = document.querySelectorAll(".squareDiv");
+  squares.forEach((squareHover) => {
+    squareHover.addEventListener("mouseover", () => {
+      squareHover.style.backgroundColor = "black";
+    });
   });
-});
+}
