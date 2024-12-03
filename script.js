025a1017bd4d676056885060c0a7c05f15723982
Gianfranco <120363150+cuzzi06@users.noreply.github.com>
2024-12-03 11:37:10 -0500

    Crear funcionalidad básica de generación de grid y efecto hover
    - Genera un grid de 16x16 cuadrados dinámicamente.
    - Añade funcionalidad de hover para cambiar el color de los cuadrados al pasar el mouse.

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+//Selector de container en index
 const containerDiv = document.querySelector("#container");
 
+//Funcion para hacer filas de 16 cuadrados
 function rowSquereDiv() {
   for (let i = 1; i <= 16; i++) {
     const gridSquareDiv = document.createElement("div");
@@ -8,6 +10,15 @@ function rowSquereDiv() {
   }
 }
 
+//Función para hacer 16 filas
 for (let i = 1; i <= 16; i++) {
   rowSquereDiv();
 }
+
+//Hover
+const squares = document.querySelectorAll(".squareDiv");
+squares.forEach((squareHover) => {
+  squareHover.addEventListener("mouseover", () => {
+    squareHover.style.backgroundColor = "black";
+  });
+});
