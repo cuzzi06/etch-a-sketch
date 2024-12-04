d3eef29cfed7d9c8b92e8aa1c5528050c32576d1
Gianfranco <120363150+cuzzi06@users.noreply.github.com>
2024-12-03 19:38:44 -0500

    Add hover effect with opacity progression and color persistence
    - Implemented functionality to progressively darken squares with each hover, using the opacity attribute.
    - Ensured that squares only change color once, maintaining their initial color for subsequent opacity updates.
    - Added data attributes for tracking color and opacity states.
    - Adjusted grid size dynamically based on user input for a better user experience.

--- script.js
+++ script.js
@@ -23,6 +23,8 @@ promptButton.addEventListener("click", () => {
       for (let i = 1; i <= numberGridSize * numberGridSize; i++) {
         const gridSquareDiv = document.createElement("div");
         gridSquareDiv.setAttribute("class", "squareDiv");
+        gridSquareDiv.setAttribute("data-changed", "false");
+        gridSquareDiv.setAttribute("data-opacity", "0");
         containerDiv.appendChild(gridSquareDiv);
       }
       adjustGridSize(numberGridSize);
@@ -36,14 +38,29 @@ promptButton.addEventListener("click", () => {
 });
 
 //Hover
+
 function assignHoverEffect() {
   const squares = document.querySelectorAll(".squareDiv");
   squares.forEach((squareHover) => {
     squareHover.addEventListener("mouseover", () => {
-      let r = Math.floor(Math.random() * 255 + 1);
-      let g = Math.floor(Math.random() * 255 + 1);
-      let b = Math.floor(Math.random() * 255 + 1);
-      squareHover.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+      let currentOpacity = parseFloat(squareHover.getAttribute("data-opacity"));
+
+      if (squareHover.dataset.changed === "false") {
+        let r = Math.floor(Math.random() * 255 + 1);
+        let g = Math.floor(Math.random() * 255 + 1);
+        let b = Math.floor(Math.random() * 255 + 1);
+        squareHover.dataset.color = `${r}, ${g}, ${b}`;
+        squareHover.dataset.changed = "true";
+        squareHover.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+      }
+
+      const getBaseColor = squareHover.dataset.color;
+
+      if (currentOpacity < 1) {
+        currentOpacity += 0.1;
+        squareHover.setAttribute("data-opacity", currentOpacity.toFixed(1));
+        squareHover.style.backgroundColor = `rgba(${getBaseColor}, ${currentOpacity})`;
+      }
     });
   });
 }
